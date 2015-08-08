6625b5ac65e0fa72b21a09a08367b74f1a249628
sfan5 <sfan5@live.de>
2015-08-08 16:01:39 +0200

    CSV is hard, fix convert-csv.py

--- util/convert-csv.py
+++ util/convert-csv.py
@@ -2,14 +2,44 @@
 import re
 import json # turns out the dump function of the json5 module just calls the normal json module （╯°□°）╯︵ ┻━┻
 
+
 INPUT = "cards-DevOpsAgainstHumanity.csv"
 META_NAME = "DevOps Against Humanity"
-DEL = ","
-LSKIP = 2
-TRANSFORMS = [
-	(r'"+', r'"')
-]
+DELIM = ","
+QUOTE = "\""
+SKIPLINES = 2
 
+
+def parse_csv(line):
+	a = []
+	tmp = ""
+	at_elem_start = True
+	in_quotes = False
+	in_escape = False
+	for c in line:
+		if at_elem_start:
+			if c == DELIM: # empty element
+				a.append("")
+				continue
+			in_quotes = (c == QUOTE)
+			if not in_quotes:
+				tmp += c
+			at_elem_start = False
+			continue
+		if c == QUOTE and in_quotes and not in_escape:
+			in_escape = True
+		elif c == QUOTE and in_quotes and in_escape:
+			tmp += QUOTE
+			in_escape = False
+		elif (c == DELIM and in_quotes and in_escape) or (c == DELIM and not in_quotes):
+			a.append(tmp)
+			tmp = ""
+			in_escape = False
+			at_elem_start = True
+		else:
+			tmp += c
+	a.append(tmp)
+	return a
 
 r_blank = re.compile(r"_+")
 odict = {}
@@ -17,35 +47,32 @@ odict["questions"] = []
 odict["answers"] = []
 odict["meta"] = {}
 odict["meta"]["name"] = META_NAME
+
 ifd = open(INPUT, "r")
-for i in range(LSKIP):
+for i in range(SKIPLINES):
 	ifd.readline()
 n = 0
+
 while True:
 	l = ifd.readline()
 	if not l:
 		break
 	l = l.rstrip("\r\n")
-	pos = l.find(",")
-	l = (l[:pos], l[pos+1:])
+	l = parse_csv(l)
 	if l[0] != "":
-		txt = l[0]
-		for t in TRANSFORMS:
-			txt = re.sub(t[0], t[1], txt)
-		odict["answers"].append(txt)
+		odict["answers"].append(l[0])
 		n += 1
 	if l[1] != "":
-		txt = l[1]
-		txt = re.sub(r_blank, "%s", txt)
-		for t in TRANSFORMS:
-			txt = re.sub(t[0], t[1], txt)
 		tmp = {}
-		tmp["text"] = txt # pick is inferred from number of %s
+		tmp["text"] = re.sub(r_blank, "%s", l[1])
+		# pick is inferred from number of %s
 		odict["questions"].append(tmp)
 		n += 1
+
 ifd.close()
 ofd = open(INPUT.replace(".csv", ".json5"), "w")
 json.dump(odict, ofd, indent=2, sort_keys=True)
 ofd.close()
+
 print("Processed %d cards." % (n, ))
 
